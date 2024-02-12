d661c99aa49e067e608c1830b45ce155209bb391
nivid24 <158420078+nivid24@users.noreply.github.com>
2024-02-12 10:21:23 +0530

    Add files via upload

---
+++ steadystatecode.m
@@ -0,0 +1,26 @@
+L=1;
+N=7;
+dx=L/(N-1);
+k = 1;
+Q = 0;
+A = zeros(N,N);
+A(1,1) = 2;
+A(N,N)=2
+for i = 2:N-1
+    A(i-1,i)=-1
+    A(i,i-1) =-1
+    A(i,i) = 2
+    A(i,i+1) =-1
+    A(N,N-1) = -1
+end
+disp(A)
+ b = zeros(N,1);
+ b(1) = 100;
+ b(N) = 10;
+ T = inv(A)*b;
+ x = linspace(0,L,N);
+ plot(x,T,'g', 'linewidth' , 1.5);
+ xlabel('Distance(m)');
+ ylabel('Temperature(c)');
+ title('Temperature Distribution')
+ grid on;
